0bce6d6d5abc2ec5de24dbe1b4d7bf8a7f3c021e
minusno0708 <naoyao1014@gmail.com>
2024-04-28 22:51:00 +0900

    データの読み込みをデータセットクラス内で実行する

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -2,41 +2,43 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 data_dir = "dataset/"
-seq_len = 96
-scaler = StandardScaler()
 
-def data_loader(file):
-    # csv読み込み
-    df_raw = pd.read_csv(data_dir + file)
+class Dataset:
+    def __init__(self, file):
+        self.seq_len = 96
+        self.scaler = StandardScaler()
+        self.data_loader(file)
 
-    # データのラベルの取得
-    cols = list(df_raw.columns)
-    cols.remove("date")
-    cols.remove("OT")
+    def data_loader(self, file):
+        # csv読み込み
+        df_raw = pd.read_csv(data_dir + file)
 
-    df_raw = df_raw[['date'] + cols + ["OT"]]
+        # データのラベルの取得
+        cols = list(df_raw.columns)
+        cols.remove("date")
+        cols.remove("OT")
 
-    # データを分割
-    num_train = int(len(df_raw) * 0.7)
-    num_test = int(len(df_raw) * 0.2)
-    num_vali = len(df_raw) - num_train - num_test
+        df_raw = df_raw[['date'] + cols + ["OT"]]
 
-    cols_data = df_raw.columns[1:]
-    df_data = df_raw[cols_data]
+        # データを分割
+        num_train = int(len(df_raw) * 0.7)
+        num_test = int(len(df_raw) * 0.2)
+        num_vali = len(df_raw) - num_train - num_test
 
-    # データの正規化
-    train_data = df_data[:num_train]
-    scaler.fit(train_data)
-    data = scaler.transform(df_data.values)
-    
-    data_x = data[:num_train]
-    data_y = data[:num_train]
+        cols_data = df_raw.columns[1:]
+        df_data = df_raw[cols_data]
 
-    return data_x, data_y
+        # データの正規化
+        train_data = df_data[:num_train]
+        self.scaler.fit(train_data)
+        data = self.scaler.transform(df_data.values)
+        
+        self.data_x = data[:num_train]
+        self.data_y = data[:num_train]
 
 
 if __name__ == "__main__":
-    x, y = data_loader("exchange_rate.csv")
-    print(x.shape)
-    print(y.shape)
+    dataset = Dataset("exchange_rate.csv")
+    print(dataset.data_x.shape)
+    print(dataset.data_y.shape)
    
